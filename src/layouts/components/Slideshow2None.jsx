2eb03f345fb9101846a3efeca074c7eed3962fc5
leonogas <afonso_@msn.com>
2023-10-08 11:11:23 +0100

    Update Slideshow2None.jsx

--- src/layouts/components/Slideshow2None.jsx
+++ src/layouts/components/Slideshow2None.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "./App.css";
+import "./app.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import ProductNone from "./ProductNone";
